aab39c12c7c81f200c1f019af55ec6458ef28fbd
ArjunRai1 <arjunrai2214@gmail.com>
2025-04-26 23:04:22 +0530

    final changes

--- recognition.py
+++ recognition.py
@@ -2,12 +2,12 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-while(True):
-    print("Start speaking: ")
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration=0.2)
-        audio = r.listen(source)
-        text = r.recognize_google(audio)
-        text = text.lower()
-        print(text)
-        break
+def voice_to_text():
+    while(True):
+        print("Start speaking: ")
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source, duration=0.2)
+            audio = r.listen(source)
+            text = r.recognize_google(audio)
+            text = text.lower()
+            return text
